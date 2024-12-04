944ed954a69040ae705540e3997313275f571793
Omar7001-B <benjami4n@gmail.com>
2024-12-05 00:55:09 +0200

    ...

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3001';
+const SOCKET_URL = "http://neko.pylex.xyz:10303";
 
 //console.log('SOCKET_URL', SOCKET_URL);
 

793f3b2e8832e528671d5684b4b0bc28e0aaba63
Omar7001-B <benjami4n@gmail.com>
2024-12-05 00:49:59 +0200

    ✂️ chore: updated env variables to import.meta.env

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:3001';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3001';
+
+//console.log('SOCKET_URL', SOCKET_URL);
 
 export const socket = io(SOCKET_URL);
 
